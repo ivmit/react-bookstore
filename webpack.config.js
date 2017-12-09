d4d32687891f8b84bae59d699c6756b2fdfde848
Ionut-Valentin Mitrache <ionutvalentin66@gmail.com>
2017-12-09 13:45:32 +0200

    set sekelton

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.tsx'
+  ],
+  output: {
+      filename: 'bundle.js',
+      publicPath: '/static/',
+      path: `${__dirname}/dist`
+  },
+  devtool: 'source-map',
+  resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  module: {
+      rules: [
+          { 
+            test: /\.tsx?$/, 
+            exclude: /node_modules/,
+            loaders: ['react-hot-loader/webpack', 'awesome-typescript-loader', 'tslint-loader']
+          },
+          { 
+            enforce: 'pre',
+            test: /\.js$/,
+            loader: 'source-map-loader'
+          }
+      ]
+  },
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    historyApiFallback: true,
+    hot: true
+  }
+};
